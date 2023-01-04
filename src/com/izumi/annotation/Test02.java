75ee1aa6c1de5e3a239c2d5dca4aaf621fcfdb9c
konglong <1016361268@qq.com>
2023-01-04 16:06:48 +0800

    元注解

---
+++ src/com/izumi/annotation/Test02.java
@@ -0,0 +1,25 @@
+package com.izumi.annotation;
+
+import java.lang.annotation.*;
+
+// 元注解
+@MyAnnotation
+public class Test02 extends Object {
+    // @MyAnnotation
+    public void test() {
+
+    }
+}
+
+// 定义一个注解
+// @Target 表示我们的注解可以用在哪些地方
+@Target(value = {ElementType.METHOD, ElementType.TYPE})
+// @Retention 表示注解在什么时候才有效，return > class > sources
+@Retention(value = RetentionPolicy.RUNTIME)
+// @Documented 表示是否将我们的注解生成在JAVAdoc中
+@Documented
+// @Inherited 表示子类可以继承父类的注解
+@Inherited
+@interface MyAnnotation {
+
+}
