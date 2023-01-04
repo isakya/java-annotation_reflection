8b3939d474d761de93e614b9555eb1e110da63a8
konglong <1016361268@qq.com>
2023-01-04 15:56:42 +0800

    内置注解

--- src/com/izumi/annotation/Test01.java
+++ src/com/izumi/annotation/Test01.java
@@ -1,11 +1,30 @@
 package com.izumi.annotation;
 
-// 什么是注解
+import java.util.ArrayList;
+import java.util.List;
+
+// 内置注解
 public class Test01 extends Object {
 
     // @Override 重写的注解
     @Override
     public String toString() {
         return super.toString();
+    }
+
+    // @Deprecated 废弃注解，jdk9后不推荐程序员使用，但是可以使用，或者存在更好的方式
+    @Deprecated
+    public static void test() {
+        System.out.println("Deprecated");
+    }
+    // @SuppressWarnings 镇压警告
+    @SuppressWarnings("all")
+    public void test02() {
+        List list = new ArrayList();
+
+    }
+
+    public static void main(String[] args) {
+        test();
     }
 }
